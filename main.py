d9c371f629fec886eade28acaee4959cc08b8c17
Lisa Santarossa <santarossa.lisa@gmail.com>
2024-09-27 12:03:45 +0100

    fix everything

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 from sklearn import linear_model
-from sklearn.linear_model import Ridge, Lasso
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV, train_test_split
 from scipy import stats
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 ## DOMANDE Friday
 # 1. correct to use only y_train_train after splitting y_train?
@@ -46,116 +43,132 @@ X_test = np.load('./input/X_test.npy')      # Input  of the test set
 
 # TODO boxplot, x_i vs y to explain lasso stuff
 
-# Create test and training set
-X_train_train, X_train_test, \
-    y_train_train, y_train_test = train_test_split(X_train, y_train, test_size=0.2, random_state=14)
+'''
+# Normalization
+X_train_train = X_train_train /np.linalg.norm(X_train_train)
+y_train_train = y_train_train /np.linalg.norm(y_train_train)
+X_train_test = X_train_test /np.linalg.norm(X_train_test)
+y_train_test = y_train_test /np.linalg.norm(y_train_test)
+'''
 
 ## 1. OUTLAIER REMOVAL
 # 1.1 Z-score method
-z_scores = np.abs(stats.zscore(y_train_train))
+z_scores = np.abs(stats.zscore(y_train))
 threshold = 3
 non_outlier_indices = np.where(z_scores <= threshold)[0]
 
-print("Cleaned set shape after Z-score:",non_outlier_indices.shape[0]) 
+print("Number of inliers after Z-score:",non_outlier_indices.shape[0]) 
 #Z-score isn't helpful! It is not suitable when data aren't well distributed    
 
 # 1.2 Interquartile Range (IQR) method 
-Q1 = np.percentile(y_train_train, 25)
-Q3 = np.percentile(y_train_train, 75)
+Q1 = np.percentile(y_train, 25)
+Q3 = np.percentile(y_train, 75)
 IQR = Q3 - Q1
 lower_bound = Q1 - 1.5 * IQR
 upper_bound = Q3 + 1.5 * IQR
-non_outlier_indices = np.where((y_train_train >= lower_bound) & (y_train_train <= upper_bound))[0]
-outlier_indices = np.where((y_train_train < lower_bound) | (y_train_train > upper_bound))
+non_outlier_indices = np.where((y_train >= lower_bound) & (y_train <= upper_bound))[0]
+outlier_indices = np.where((y_train < lower_bound) | (y_train > upper_bound))
 
-print("Cleaned set shape after IRQ:",non_outlier_indices.shape[0])
+print("Number of inliers after IRQ:",non_outlier_indices.shape[0])
+#Not that helpful  
 
 # 1.3 MAD-median rule method
-non_outlier, outliers, median, mad, madn = mad_median_outlier_detection(y_train_train)
+non_outlier, outliers, median, mad, madn = mad_median_outlier_detection(y_train)
 
-X_train_train_clean = X_train_train[non_outlier]
-y_train_train_clean = y_train_train[non_outlier]
+print("Number of inliers after MAD:", len(non_outlier))
 
-print(f"Outleir removal with MAD method:----------------------------")
-print("Cleaned set shape after MAD:", len(non_outlier))
-print("Median:", median)
-print("MAD:", mad)
-print("MADN:", madn)
+# 1.4 RANSAC regression and then outlier removal
+ransac = linear_model.RANSACRegressor(random_state=190)
+ransac.fit(X_train, y_train)
+y_pred_ransac = ransac.predict(X_train)
+inlier_mask = ransac.inlier_mask_
+X_train_clean = X_train[inlier_mask]
+y_train_clean = y_train[inlier_mask]
+print(f"Number of inliers after RANSAC: {np.sum(inlier_mask)}")
 
-non_outlier, outliers, median, mad, madn = mad_median_outlier_detection(y_train_test)
-X_train_test_clean = X_train_test[non_outlier]
-y_train_test_clean = y_train_test[non_outlier]
+# Create test and training set
+X_train_clean_train, X_train_clean_test, \
+    y_train_clean_train, y_train_clean_test = train_test_split(X_train_clean, y_train_clean, test_size=0.2, random_state=14)
 
-plt.scatter(np.arange(y_train_train_clean.shape[0]), y_train_train_clean, color='blue', label='Non-Outliers')
-plt.scatter(np.arange(y_train_train.shape[0]), y_train_train, color='red', label='Outliers')
-plt.title('Visualization of Outliers in y_train')
-plt.xlabel('Sample index')
-plt.ylabel('Concentration of toxic algae')
-plt.show()
+non_outlier, outliers, median, mad, madn = mad_median_outlier_detection(y_train_clean)
 
 ## 2. LINEAR REGRESSION
 regr = linear_model.LinearRegression()
-regr.fit(X_train_train_clean, y_train_train_clean)
-y_pred_train_test = regr.predict(X_train_test_clean) 
+regr.fit(X_train_clean_train, y_train_clean_train)
+y_pred_train_test = regr.predict(X_train_clean_test)
 
-sse = calculate_sse(y_train_test_clean, y_pred_train_test)
-r2 = calculate_r2(y_train_test_clean, y_pred_train_test)
-print(f"Linear regression:------------------------------------------")
-print(f"Coefficients:", regr.coef_)
-print(f"Intercept:", regr.intercept_)
-print(f"SSE: {sse}")
-print(f"R^2: {r2}")
+sse = calculate_sse(y_train_clean_test, y_pred_train_test)
+r2 = r2_score(y_train_clean_test, y_pred_train_test)
+#print(f"Coefficients:", regr.coef_)
+#print(f"Intercept:", regr.intercept_)
+print(f"Linear Regression SSE: {sse}")
+print(f"Linear Regression R^2: {r2}")
 
 feature_names = ['Air Temp (x1)', 'Water Temp (x2)', 'Wind Speed (x3)', 'Wind Dir (x4)', 'Illumination (x5)']
-for i in range(X_train_test_clean.shape[0]):
-    plt.scatter(X_train_test_clean[:, i], y_pred_train_test, color='orange', label='Prediction')
-    plt.scatter(X_train_test_clean[:, i], y_train_test_clean, color='blue', label='Test set')
+for i in range(len(feature_names)):
+    plt.scatter(X_train_clean_test[:, i], y_pred_train_test, color='orange', label='Prediction')
+    plt.scatter(X_train_clean_test[:, i], y_train_clean_test, color='blue', label='Test set')
     plt.xlabel(feature_names[i])
     plt.ylabel('Toxic Algae Concentration (y)')
     plt.title(f'{feature_names[i]} vs Toxic Algae Concentration')
     plt.legend()
     plt.show()
-
 '''
-## 3. RE    
+v1 = np.linspace(0, 0.3, 1000)
+v2 = np.linspace(0, 0.3, 1000)
+v3 = np.linspace(0, 0.3, 1000)
+v4 = np.linspace(0, 0.3, 1000)
+v5 = np.linspace(0, 0.3, 1000)
+input = np.zeros(shape=[1000,5])
+input[:,0]=v1
+input[:,1]=v2
+input[:,2]=v3
+input[:,3]=v4
+input[:,4]=v5
+
+y_pred_train_test = regr.predict(input)
+feature_names = ['Air Temp (x1)', 'Water Temp (x2)', 'Wind Speed (x3)', 'Wind Dir (x4)', 'Illumination (x5)']
+for i in range(len(feature_names)):
+    plt.scatter(input[:, i], y_pred_train_test, color='orange', label='Prediction')
+    plt.scatter(X_train_clean_test[:, i], y_train_clean_test, color='blue', label='Test set')
+    plt.xlabel(feature_names[i])
+    plt.ylabel('Toxic Algae Concentration (y)')
+    plt.title(f'{feature_names[i]} vs Toxic Algae Concentration')
+    plt.legend()
+    plt.show()
+'''
+
+## 3. REGULARIZATION
 # Ridge Model
-ridge = Ridge()
+ridge = linear_model.Ridge()
 alpha_range = np.logspace(-4, 4, 50)
 ridge_cv = GridSearchCV(ridge, param_grid={'alpha': alpha_range}, cv=5)
-ridge_cv.fit(X_train_clean, y_train_clean)
+ridge_cv.fit(X_train_clean_train, y_train_clean_train)
 print(f"Best alpha for Ridge: {ridge_cv.best_params_['alpha']}")
-y_pred_ridge = ridge_cv.predict(X_train_clean)
+y_pred_ridge = ridge_cv.predict(X_train_clean_test)
+sse = calculate_sse(y_train_clean_test,y_pred_ridge)
+print("Ridge SSE: ", sse)
 
 # Lasso Regression
-lasso = Lasso(alpha=0.1)
-lasso.fit(X_train_clean, y_train_clean)
-y_pred_lasso = lasso.predict(X_train_clean)
-
-
-alphas = np.logspace(-4, 4, 10) # values to try
-mean_scores = [] # mean cross-validation scores for each alpha
-
-# Loop over each alpha, apply cross-validation, and store the result
-for alpha in alphas:
-    # Initialize the Lasso model with the current alpha
-    lasso = Lasso(alpha=alpha, max_iter=10000)
+lasso = linear_model.LassoCV(cv=15, random_state=190)
+lasso.fit(X_train_clean_train, y_train_clean_train)
+alpha_lasso = lasso.alpha_
+beta_lasso = lasso.coef_
+print(f"Best alpha: {lasso.alpha_}")
+y_pred_lassoCV = lasso.predict(X_train_clean_test)
+sse = calculate_sse(y_train_clean_test,y_pred_lassoCV)
+print("Lasso SSE: ", sse)
     
-    # Perform 5-fold cross-validation
-    # Using cross_val_score to evaluate the model's performance
-    # scoring='neg_mean_squared_error' because Lasso minimizes squared error
-    scores = cross_val_score(lasso, X_train_clean, y_train_clean, cv=5, scoring='neg_mean_squared_error')
-    
-    # Compute the mean of the cross-validated scores
-    mean_score = np.mean(scores)
-    mean_scores.append(mean_score)
+# Lasso Regression with grid search cv
+lasso = linear_model.Lasso()
+param_grid = {'alpha': [1, 10, 100]}
+grid_search = GridSearchCV(estimator=lasso, param_grid=param_grid, 
+                           scoring='neg_mean_squared_error', cv=15)
+grid_search.fit(X_train_clean_train, y_train_clean_train)
+alpha_lasso = grid_search.best_params_['alpha']
+print(f"Best alpha from GridSearchCV: {alpha_lasso}")
+best_lasso = grid_search.best_estimator_
+y_pred_lasso = best_lasso.predict(X_train_clean_test)
+sse = calculate_sse(y_train_clean_test, y_pred_lasso)
+print("Lasso SSE with GridSearchCV: ", sse)
 
-# Convert mean_scores list to numpy array
-mean_scores = np.array(mean_scores)
-
-# Find the best alpha (the one with the highest mean cross-validation score)
-best_alpha = alphas[np.argmax(mean_scores)]
-
-# Output the best alpha and its corresponding score
-print(f"Best alpha: {best_alpha}")
-#print(f"Best score: {mean_scores[np.argmax(mean_scores)]}")
-'''
