e2d755721e1abc8dead8f15e4074f24426da5910
Lisa Santarossa <santarossa.lisa@gmail.com>
2024-10-07 11:02:10 +0100

    first commit

---
+++ Part-03/main3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Load data
+X_test1 = np.load('./input/Xtest1.npy')
+X_train1 = np.load('./input/Xtrain1.npy')
+X_train1_extra = np.load('.input/Xtrain1_extra.npy')  
+y_train1 = np.load('./input/ytrain1.npy')
+y_train1_extra = np.load('.input/ytrain1_extra.npy')  
